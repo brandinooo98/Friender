4fff5614d233e5af5b56a3a15bd9b46db8859a8f
Brandinooo98 <brandinooo98@gmail.com>
2019-11-26 16:43:03 -0600

    Init

---
+++ src/Graph.java
@@ -0,0 +1,45 @@
+import java.util.List;
+import java.util.Set;
+
+public class Graph implements GraphADT{
+
+    @Override
+    public void addVertex(String vertex) {
+
+    }
+
+    @Override
+    public void removeVertex(String vertex) {
+
+    }
+
+    @Override
+    public void addEdge(String vertex1, String vertex2) {
+
+    }
+
+    @Override
+    public void removeEdge(String vertex1, String vertex2) {
+
+    }
+
+    @Override
+    public Set<String> getAllVertices() {
+        return null;
+    }
+
+    @Override
+    public List<String> getAdjacentVerticesOf(String vertex) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int order() {
+        return 0;
+    }
+}
